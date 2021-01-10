f554ea4a984554424a4bceba840939cb008622d2
pavels1982@mail.ru <pavels1982@mail.ru>
2021-01-10 20:33:47 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace NeAptekaBot
+{
+    class Program
+    {
+        private static ITelegramBotClient client;
+        static void Main(string[] args)
+        {
+            client = new TelegramBotClient("1506064902:AAH1x0iPMJsZIrb1elwSDzgCyejh2IXvFZo") { Timeout = TimeSpan.FromSeconds(10) };
+            var me = client.GetMeAsync().Result;
+            Console.WriteLine(me.FirstName + " id: " + me.Id ) ;
+            client.OnMessage += Client_OnMessage;
+            client.StartReceiving();
+            while (true) Thread.Sleep(10000);
+            //Console.ReadKey();
+        }
+
+        private static async void Client_OnMessage(object sender, MessageEventArgs e)
+        {
+            var text = e?.Message?.Text;
+            if (text == null) return;
+            Console.WriteLine($"recived text '{text}' in chat '{e.Message.Chat.Id}'");
+
+            await client.SendTextMessageAsync(
+                chatId: e.Message.Chat,
+                text: $"Привет '{text}'"
+                ).ConfigureAwait(false);
+
+        }
+    }
+}
